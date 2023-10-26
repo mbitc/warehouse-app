d28c146bb88869fa6da74cd42184b448d5827b75
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-10-26 16:57:13 +0300

    change style for dashboard

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -3,30 +3,30 @@ import { useEffect, useState } from 'react';
 import Container from '../../components/Container/Container';
 import ProductsCount from '../../components/ProductsCount/ProductsCount';
 import { ROOT_PATH } from '../../config';
+import style from './DashboardPage.module.scss';
 
 const DashboardPage = () => {
-    const navigate = useNavigate();
-    const [loged, setLoged] = useState(false);
+  const navigate = useNavigate();
+  const [loged, setLoged] = useState(false);
 
-    const browserDataLoged = localStorage.getItem('loged');
+  const browserDataLoged = localStorage.getItem('loged');
 
-    useEffect(() => {
-        setLoged(browserDataLoged)
-    }, [browserDataLoged])
+  useEffect(() => {
+    setLoged(browserDataLoged);
+  }, [browserDataLoged]);
 
-    if (!loged) {
-        navigate(`${ROOT_PATH}`)
-    }
+  if (!loged) {
+    navigate(`${ROOT_PATH}`);
+  }
 
-    return (
-        <Container>
-            <h2>Dashboard</h2>
-            <div>
-                <ProductsCount />
-                <span className='test'>At The Warehouse are Items</span>
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <div className={style.dashboardWrapper}>
+        <ProductsCount />
+        <span>At The Warehouse are Items</span>
+      </div>
+    </Container>
+  );
 };
 
 export default DashboardPage;
