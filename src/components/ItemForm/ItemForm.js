874e1c9d264f211ef54f29c84fd642e5ecbea4cf
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-10-28 02:07:13 +0300

    fix

--- src/components/ItemForm/ItemForm.js
+++ src/components/ItemForm/ItemForm.js
@@ -139,7 +139,7 @@ const ItemForm = ({ show, onCloseModal, data }) => {
               value={newItem.img}
               onChange={inputsHandler}
             />
-            <button className='camera' onClick={() => closeModalHandler} />
+            <button className='camera' onClick={closeModalHandler} />
           </div>
         </div>
         <div className='form-control'>
