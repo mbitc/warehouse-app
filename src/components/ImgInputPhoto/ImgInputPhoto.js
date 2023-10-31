94287113d29e0a632d5b532401143ad67aa6cc5f
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-11-01 01:40:14 +0200

    style cam

--- src/components/ImgInputPhoto/ImgInputPhoto.js
+++ src/components/ImgInputPhoto/ImgInputPhoto.js
@@ -4,8 +4,8 @@ import style from './ImgInputPhoto.module.scss';
 const ImgInputPhoto = ({ show, closeImgCamModal }) => {
   return (
     <dialog className={style.videoWrapp} open={show}>
-      <button className='delete' onClick={closeImgCamModal} />
       <WebCam />
+      <button className='delete' onClick={closeImgCamModal} />
     </dialog>
   );
 };
