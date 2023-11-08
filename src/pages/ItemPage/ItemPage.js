0c97ffd9429bc5043916a57d235b25a6abb90da7
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-11-08 18:24:42 +0200

    add color for price

--- src/pages/ItemPage/ItemPage.js
+++ src/pages/ItemPage/ItemPage.js
@@ -46,7 +46,7 @@ const ItemPage = () => {
           <div className={style.itemData}>
             <span>{item.code}</span>
             <span>{item.qty} unit</span>
-            <span>€ {item.price}</span>
+            <span className={style.price}>€ {item.price}</span>
             {storageListElement}
             <Link className='link' to={`${ROOT_PATH}catalog/${item.catalogId}`}>
               <span className={style.backLink}>back to products list</span>
